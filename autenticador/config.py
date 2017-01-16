a5eb47fc8747b893a7087a58b6602c96545bbb89
Oskar <oskar.black5862@gmail.com>
2017-01-16 02:22:28 -0600

    Agregando modelo

---
+++ autenticador/config.py
@@ -0,0 +1,8 @@
+import os
+class Config(object):
+    SECRET_KEY = 'my_secret_key'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLAlCHEMY_DATABASE_URI = 'mysql://root:@localhost/flask'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- autenticador/main.py
+++ autenticador/main.py
@@ -1,14 +1,32 @@
-from flask import Flask,request,render_template,make_response,session,redirect,url_for
+from flask import Flask,request,render_template,make_response,session,redirect,url_for,flash,g
 from flask_wtf import CsrfProtect
 
+from config import DevelopmentConfig
+from models import db
+from models import User
+
+import json
 import forms
 
 app = Flask(__name__) #nuevo objeto
-app.secret_key = 'my_secret_key'
-csrf = CsrfProtect(app)
+app.config.from_object(DevelopmentConfig)
+
+csrf = CsrfProtect()
+
+@app.before_request
+def before_request():
+    g.test = 'test1'
+    if 'username' not in session:
+        print 'El usuario necesita autenticarse'
+
+@app.after_request
+def after_request(response):
+    print g.test
+    return response
 
 @app.route('/')
 def index():
+    print g.test
     #custome_cookie = request.cookies.get('custome_cookie','Undefined')
     #print custome_cookie
     if 'username' in session:
@@ -20,6 +38,9 @@ def index():
 def login():
     login_form = forms.LoginForm(request.form)
     if request.method == 'POST' and login_form.validate():
+        username = login_form.username.data
+        success_message = 'Bienvenido {}'.format(username)
+        flash(success_message)
         session['username'] = login_form.username.data
     return render_template('login.html', form=login_form)
 
@@ -48,8 +69,20 @@ def comment():
 
     return render_template('comment.html', form = comment_form)
 
+@app.route('/ajax-login', methods=['POST'])
+def ajax_login():
+    print request.form
+    username = request.form['username']
+    response = {'status':200,'username':username, 'id':1}
+    return json.dumps(response)
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
+
 if __name__ == '__main__':
-	app.run(debug = True, port = 8000) #se encarga de ejecutar el servidor
+    csrf.init_app(app)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.run(port = 8000) #se encarga de ejecutar el servidor
