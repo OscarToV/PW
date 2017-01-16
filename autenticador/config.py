713e1b4f4615f2cb4ea41f700e9eefef70209061
yes_240894@hotmail.com <yes_240894@hotmail.com>
2017-01-16 11:38:04 -0600

    Modificación config.py

--- autenticador/config.py
+++ autenticador/config.py
@@ -3,6 +3,6 @@ class Config(object):
     SECRET_KEY = 'my_secret_key'
 
 class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLAlCHEMY_DATABASE_URI = 'mysql://root:@localhost/flask'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+ 	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/flask'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
